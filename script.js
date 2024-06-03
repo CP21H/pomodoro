5de0ddd965e2164943c78b5bdf38fe207b6cade1
CP21H <cp21h@fsu.edu>
2024-06-03 10:07:22 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function changeWallpaper(id) {
+    if (id == "wall1") {
+        document.body.style.backgroundImage = "url(images/wall1.jpg)";
+    }
+    if (id == "wall2") {
+        document.body.style.backgroundImage = "url(images/wall2.jpg)";
+    }
+    if (id == "wall3") {
+        document.body.style.backgroundImage = "url(images/wall3.jpg)";
+    }
+    if (id == "wall4") {
+        document.body.style.backgroundImage = "url(images/wall4.jpg)";
+    }
+    if (id == "wall5") {
+        document.body.style.backgroundImage = "url(images/wall5.jpg)";
+    }
+    if (id == "wall6") {
+        document.body.style.backgroundImage = "url(images/wall6.jpg)";
+    }
+}
